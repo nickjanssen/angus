9fad7f238955a1b2ac12d1a4b569ccdaa686fac4
Nick Janssen <info@nickjanssen.com>
2014-08-06 23:23:53 +0200

    feat(replace): add @@libScripts to have sorted lib scripts instead of using grunt-include-source for them (was unsorted)

--- grunt/replace.js
+++ grunt/replace.js
@@ -1,11 +1,24 @@
 'use strict';
 
+var buildConfig = require('../nconf.js');
+var appConfig = require('../src/' + buildConfig.get('app') + '/config.js');
+
+// Build an array of script tags manually
+// We can't use grunt-include-source because it does not take into account
+// the order of the libIncludes.js array in /src/your-app/config.js
+var libScripts = appConfig.libIncludes.js
+.map(function (script) {
+    return '<script src="' + (appConfig.staticServerUrl || '') + 'assets/js/lib/' + script + '"></script>';
+}).join('\n');
+
+
 module.exports = {
     dev: {
         options: {
             variables: {
                 app: '<%= cfg.app %>',
-                minified: ''
+                minified: '',
+                libScripts: libScripts
             }
         },
         files: [{
@@ -21,7 +34,8 @@ module.exports = {
         options: {
             variables: {
                 app: '<%= cfg.app %>',
-                minified: '.min'
+                minified: '.min',
+                libScripts: libScripts
             }
         },
         files: [{
