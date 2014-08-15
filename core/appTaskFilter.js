c66efb0a53a4cb7e6822e6b6a996f2f886ecac1f
Nick Janssen <info@nickjanssen.com>
2014-08-15 17:47:48 +0200

    fix(appTaskFilter): better ignoring

--- core/appTaskFilter.js
+++ core/appTaskFilter.js
@@ -7,7 +7,11 @@ module.exports = function (angus) {
         var found = false;
         var gruntTasks = angus.appConfig.gruntTasks || require('./defaultTasks.js');
         if (angus.appConfig.gruntTasksIgnore) {
-            gruntTasks = _.without(gruntTasks, angus.appConfig.gruntTasksIgnore);
+            gruntTasks = _.filter(gruntTasks, function (task) {
+                return _.some(angus.appConfig.gruntTasksIgnore, function (ignoredTask) {
+                    return task.indexOf(ignoredTask) === -1;
+                });
+            });
         }
         if (angus.appConfig.gruntTasksAdd) {
             gruntTasks = _.union(gruntTasks, angus.appConfig.gruntTasksAdd);
