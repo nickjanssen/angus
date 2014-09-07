1bacad8d109f17bba1a4e0786a0fe6510c1a8931
Nick Janssen <info@nickjanssen.com>
2014-09-07 12:48:13 +0200

    feat(angus.js): add version information when command is run

--- angus.js
+++ angus.js
@@ -64,9 +64,11 @@ if (args.length > 0) {
         }
     }
     else {
+        console.log(gutil.colors.white('Angus v' + pkg.version));
+
         var angusLocation = cwd + '/angus.config.js';
         if (fs.existsSync(angusLocation)) {
-        require('./core/gulpfile.js')(args, cwd);
+            require('./core/gulpfile.js')(args, cwd);
         } else {
             gutil.log(gutil.colors.red('angus.config.js was not found! Please check that you are in a valid angus directory.'));
             gutil.log(gutil.colors.yellow('If you would like to create an angus application, see the ') + gutil.colors.magenta('angus create') + gutil.colors.yellow(' command.'));
