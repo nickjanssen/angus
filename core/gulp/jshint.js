abda221245a59ed2fa7ceb61dde262a0ca76f834
Nick Janssen <info@nickjanssen.com>
2014-09-30 16:35:16 +0200

    feat(jshint): if an app-specific .jshint file could not be found, use the one from angus

--- core/gulp/jshint.js
+++ core/gulp/jshint.js
@@ -2,14 +2,26 @@
 
 var jshint  = require('gulp-jshint');
 var playSound = require('../playSound.js');
+var fs = require('fs');
+var gutil = require('gulp-util');
 
 module.exports = function (angus, gulp) {
+
+    // If we cannot find a .jshintrc file, just use the one from the angus repo
+    var jshintFilePath = angus.appPath + '/.jshintrc';
+
+    if (!fs.existsSync(jshintFilePath)) {
+        gutil.log(gutil.colors.yellow('No .jshintrc file found in the root folder of your app.'));
+        gutil.log(gutil.colors.yellow('Using default angus jshint rules.'));
+        jshintFilePath = '.jshintrc';
+    }
+
     return function () {
         return gulp.src([
                 angus.appPath + '/src/core/**/*.js',
                 '!' + angus.appPath + '/src/core/**/_*.js'
             ])
-            .pipe(jshint(angus.appPath + '/.jshintrc'))
+            .pipe(jshint(jshintFilePath))
             .pipe(jshint.reporter(function () {
                 playSound('break');
             }))
