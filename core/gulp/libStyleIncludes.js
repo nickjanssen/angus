4e9da90b8b726c4c96999c8166f6f71ae7942e4b
Nick Janssen <info@nickjanssen.com>
2014-10-03 09:08:02 +0200

    fix(libStyleIncludes): force CSS files to be read as LESS files #21

--- core/gulp/libStyleIncludes.js
+++ core/gulp/libStyleIncludes.js
@@ -4,7 +4,7 @@ module.exports = function (angus) {
 
     var includes = angus.appConfig.bower.filesNeeded[angus.appConfig.__cssCompilerExtension]
     .map(function (file) {
-        return '@import \'../../bower_components/' + file + '\';';
+        return '@import ' + (angus.appConfig.__cssCompilerExtension === 'less' ? '(less) ' : '') + '\'../../bower_components/' + file + '\';';
     }).join('\n');
 
     return function (cb) {
