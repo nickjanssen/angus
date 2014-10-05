853eca0273b13bb12ea43969db3d27a7b41624b0
Nick Janssen <info@nickjanssen.com>
2014-10-05 14:27:53 +0200

    feat(watch): better watching

--- core/gulp/watch.js
+++ core/gulp/watch.js
@@ -8,13 +8,17 @@ module.exports = function (angus, gulp) {
 
     return function () {
 
+        gulp.watch(angus.appPath + '/src/assets/**/*', makeTaskList(angus, ['assets']));
         gulp.watch(angus.appPath + '/src/*.html', makeTaskList(angus, ['html']));
-        gulp.watch(angus.appPath + '/src/core/**/*.js', makeTaskList(angus, ['js', 'autoInclude']));
         gulp.watch([
+            angus.appPath + '/src/core/**/*.js',
+            angus.appPath + '/src/core/**/*.tpl.html'
+        ], makeTaskList(angus, ['js', 'autoInclude']));
+        gulp.watch([
+            angus.appPath + '/src/core/**/*.' + angus.appConfig.__cssCompilerExtension,
             angus.appPath + '/src/style/*.' + angus.appConfig.__cssCompilerExtension,
             '!' + angus.appPath + '/src/style/_includes.' + angus.appConfig.__cssCompilerExtension
         ], makeTaskList(angus, ['css']));
-        // console.log(makeTaskList(angus, ['js', 'autoInclude']));
         gulp.watch(angus.appPath + '/angus.config.js', ['configFileChangedWarning']);
 
         playSound('start');
